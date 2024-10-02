5016481471807533acac8a6a28928e524b2d3441
Egor Fedorov <fegor2004@gmail.com>
2024-10-02 20:32:51 +0300

    feat: add pagination schemas

--- frontend/src/app/routes/humans/route.tsx
+++ frontend/src/app/routes/humans/route.tsx
@@ -1,28 +1,34 @@
 import { useHumanBeingTable } from "@/entities/human-being/table";
+import { PaginatedQuerySchema } from "@/shared/pagination";
 import { DataTable } from "@/shared/ui/data-table";
 import { createFileRoute } from "@tanstack/react-router";
 
+const SearchSchema = PaginatedQuerySchema;
+
 export const Route = createFileRoute("/humans")({
   component: Page,
+  validateSearch: SearchSchema,
 });
 
 function Page() {
-  const table = useHumanBeingTable([{
-    id: 1,
-    name: "asd",
-    realHero: true,
-    mood: "CALM",
-    coordinates: {
-      x: 1,
-      y: 1
+  const table = useHumanBeingTable([
+    {
+      id: 1,
+      name: "asd",
+      realHero: true,
+      mood: "CALM",
+      coordinates: {
+        x: 1,
+        y: 1,
+      },
+      creationDate: new Date(),
+      hasToothpick: true,
+      weaponType: "AXE",
+      car: {
+        cool: true,
+      },
+      impactSpeed: 50,
     },
-    creationDate: new Date(),
-    hasToothpick: true,
-    weaponType: "AXE",
-    car: {
-      cool: true
-    },
-    impactSpeed: 50
-  }]);
+  ]);
   return <DataTable table={table} />;
 }
---
+++ frontend/src/shared/pagination.ts
@@ -0,0 +1,18 @@
+import { z, ZodType } from "zod";
+
+const ALLOWED_PAGE_SIZE = [10, 25, 50];
+const DEFAULT_PAGE_SIZE = ALLOWED_PAGE_SIZE[0];
+
+export const PaginatedQuerySchema = z.object({
+  page: z.number().min(1).catch(1),
+  pageSize: z
+    .number()
+    .refine((value) => ALLOWED_PAGE_SIZE.includes(value))
+    .catch(DEFAULT_PAGE_SIZE),
+});
+
+export const PaginatedResponseSchema = <T>(value: ZodType<T>) =>
+  z.object({
+    total: z.number(),
+    values: z.array(value),
+  });
