809d17e2116f9dbd626d104ea7d35dbde3fff311
Egor Fedorov <fegor2004@gmail.com>
2024-10-02 22:40:30 +0300

    feat: add monkey icon

--- frontend/src/app/routes/__root.tsx
+++ frontend/src/app/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import webMonkeyIcon from "@/shared/ui/icon.png";
 
 type RouterContext = {
   queryClient: QueryClient;
@@ -10,8 +11,11 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
       <header className="border-b">
-        <nav className="container mx-auto flex items-center h-14">
-          <h1 className="text-2xl font-bold">Вебмакакинг</h1>
+        <nav className="container mx-auto flex h-14 items-center">
+          <h1 className="text-2xl font-bold inline-flex items-center space-x-2">
+            <img src={webMonkeyIcon} className="size-8" />
+            Вебмакакинг
+          </h1>
         </nav>
       </header>
       <main className="container mx-auto mt-8">
