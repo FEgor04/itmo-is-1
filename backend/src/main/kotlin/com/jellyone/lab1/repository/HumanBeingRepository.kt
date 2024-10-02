dbae2ae06f05c9d19fde0c2ecaa69ab170a39a91
forafox <deskpa@yandex.ru>
2024-10-03 01:40:39 +0300

    feat(backend): create repositories

---
+++ backend/src/main/kotlin/com/jellyone/lab1/repository/HumanBeingRepository.kt
@@ -0,0 +1,36 @@
+package com.jellyone.lab1.repository
+
+import com.jellyone.lab1.domain.HumanBeing
+import org.jooq.DSLContext
+import org.jooq.impl.DSL
+import org.springframework.stereotype.Repository
+
+@Repository
+class HumanBeingRepository(private val dsl: DSLContext) {
+
+    fun findById(id: Long): HumanBeing? {
+        return dsl.selectFrom("human_being")
+            .where(DSL.field("id").eq(id))
+            .fetchOneInto(HumanBeing::class.java)
+    }
+
+    fun save(humanBeing: HumanBeing): HumanBeing {
+        val record = dsl.insertInto(DSL.table("human_being"))
+            .set(DSL.field("name"), humanBeing.name)
+            .set(DSL.field("x"), humanBeing.coordinates.x)
+            .set(DSL.field("y"), humanBeing.coordinates.y)
+            .set(DSL.field("creation_date"), humanBeing.creationDate)
+            .set(DSL.field("real_hero"), humanBeing.realHero)
+            .set(DSL.field("has_toothpick"), humanBeing.hasToothpick)
+            .set(DSL.field("car_id"), humanBeing.car.id)
+            .set(DSL.field("mood"), humanBeing.mood?.name)
+            .set(DSL.field("impact_speed"), humanBeing.impactSpeed)
+            .set(DSL.field("weapon_type"), humanBeing.weaponType.name)
+            .returning(DSL.field("id"))
+            .fetchOne()
+
+        val id = record?.get(DSL.field("id")) as Long?
+
+        return humanBeing.copy(id = id)
+    }
+}
