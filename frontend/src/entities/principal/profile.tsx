a90e64f21b7caa3d3be83ece6823418ca0aacfb3
Egor Fedorov <fegor2004@gmail.com>
2024-10-04 21:18:54 +0300

    feat: add sidebar menu (#32)

---
+++ frontend/src/entities/principal/profile.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/shared/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/shared/ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getPrincipalQueryOptions } from "./api";
+import { Link } from "@tanstack/react-router";
+import { Skeleton } from "@/shared/ui/skeleton";
+
+export function PrincipalProfile() {
+  const { data, isError } = useQuery(getPrincipalQueryOptions());
+
+  if (isError) {
+    return (
+      <Button asChild>
+        <Link to="/signin">Войти</Link>
+      </Button>
+    );
+  }
+
+  if (data == undefined) {
+    return <Skeleton className="size-8 rounded-full" />;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="ml-auto overflow-hidden rounded-full"
+        >
+          {data[0]}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>{data}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Заявки</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Выйти</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
