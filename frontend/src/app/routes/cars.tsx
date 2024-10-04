6808745ebf7d1fd4a5cf19ba35cf8d3828dd3e0a
Egor Fedorov <fegor2004@gmail.com>
2024-10-04 23:03:03 +0300

    feat: add filters by name & model

--- frontend/src/app/routes/cars.tsx
+++ frontend/src/app/routes/cars.tsx
@@ -1,10 +1,14 @@
 import { getCarsQueryOptions, GetCarsQuerySchema } from "@/entities/car/api";
 import { CarKeys } from "@/entities/car/model";
 import { useCarTable } from "@/entities/car/table";
+import { Button } from "@/shared/ui/button";
 import { DataTable } from "@/shared/ui/data-table";
+import { Input } from "@/shared/ui/input";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { SortingState } from "@tanstack/react-table";
+import { PlusCircle } from "lucide-react";
+import { useDebouncedCallback } from "use-debounce";
 import { z } from "zod";
 
 const SearchSchema = GetCarsQuerySchema;
@@ -53,8 +57,42 @@ function Page() {
       sortDirection: newValue[0].desc ? "desc" : "asc",
     }));
   });
+
+  const debouncedSetBrand = useDebouncedCallback((brand: string) => {
+    setQuery((prev) => ({
+      ...prev,
+      brand,
+    }));
+  }, 200);
+
+  const debouncedSetModel = useDebouncedCallback((model: string) => {
+    setQuery((prev) => ({
+      ...prev,
+      model,
+    }));
+  }, 200);
+
+
   return (
-    <div>
+    <div className="space-y-4">
+      <header className="flex">
+        <Button variant="outline">
+          <PlusCircle className="size-4" />
+          Создать
+        </Button>
+        <div className="ml-auto flex space-x-2">
+          <Input
+            placeholder="Брэнд"
+            defaultValue={search.brand}
+            onChange={(e) => debouncedSetBrand(e.target.value)}
+          />
+          <Input
+            placeholder="Модель"
+            defaultValue={search.model}
+            onChange={(e) => debouncedSetModel(e.target.value)}
+          />
+        </div>
+      </header>
       <main>
         <DataTable table={table} />
       </main>
