fdeb802f5fb2f6cd6c58c84229f8df46f48ff051
Karabanov Andrey <78631409+forafox@users.noreply.github.com>
2024-10-05 12:14:29 +0300

    feat(backend): implementation `GET /admin/requests/` endpoint (#53)

--- backend/src/main/kotlin/com/jellyone/lab1/mapper/RequestAdminMapper.kt
+++ backend/src/main/kotlin/com/jellyone/lab1/mapper/RequestAdminMapper.kt
@@ -2,9 +2,11 @@ package com.jellyone.lab1.mapper
 
 import com.jellyone.lab1.domain.AdminRequest
 import com.jellyone.lab1.web.dto.AdminRequestDto
+import org.springframework.stereotype.Component
 
+@Component
 class RequestAdminMapper {
     fun toDto(request: AdminRequest): AdminRequestDto {
-        return AdminRequestDto(request.username, request.password)
+        return AdminRequestDto(request.username, request.status)
     }
 }
--- backend/src/main/kotlin/com/jellyone/lab1/web/dto/AdminRequest.kt
+++ backend/src/main/kotlin/com/jellyone/lab1/web/dto/AdminRequest.kt
@@ -1,3 +1,5 @@
 package com.jellyone.lab1.web.dto
 
-data class AdminRequestDto(val username: String, val password: String)
+import com.jellyone.lab1.domain.enums.AdminRequestStatus
+
+data class AdminRequestDto(val username: String, val status: AdminRequestStatus)
