0e26df90f6abfe0a3bafc8256092263441474848
Egor Fedorov <fegor2004@gmail.com>
2024-10-05 12:15:40 +0300

    add success card

--- frontend/src/app/routes/_auth/requests.tsx
+++ frontend/src/app/routes/_auth/requests.tsx
@@ -1,9 +1,43 @@
+import { getPrincipalQueryOptions } from "@/entities/principal/api";
 import { PaginatedQuerySchema } from "@/shared/pagination";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/shared/ui/card";
 import { createFileRoute } from "@tanstack/react-router";
 
 const SearchSchema = PaginatedQuerySchema;
 
 export const Route = createFileRoute("/_auth/requests")({
-  component: () => <div>Hello /_auth/requests!</div>,
+  component: Page,
   validateSearch: SearchSchema,
+  loader: async ({ context }) => {
+    const me = await context.queryClient.ensureQueryData(
+      getPrincipalQueryOptions(),
+    );
+    return me;
+  },
 });
+
+function Page() {
+  const me = Route.useLoaderData();
+  if (me.role == "ADMIN") {
+    return <AdminPage />;
+  }
+  return <AdminPage />
+}
+
+function AdminPage() {
+  return (
+    <div>
+      <Card className="max-w-lg mx-auto bg-green-500 text-slate-50">
+        <CardHeader>
+          <CardTitle>🎉 Congratulations!</CardTitle>
+        </CardHeader>
+        <CardContent>
+            Теперь вы можете посмотреть заявки других
+            пользователей на становление администратором.
+
+            Вы также можете редактировать и удалять объекты всех пользователей.
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
