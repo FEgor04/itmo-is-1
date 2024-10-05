b41a008f2f5481f120effd0b8ee89336c7a97ba4
Karabanov Andrey <78631409+forafox@users.noreply.github.com>
2024-10-05 04:42:27 +0300

    fix(backend): remove useless annotations in model classes from jakarta.persistence (#43)

--- backend/src/main/kotlin/com/jellyone/lab1/domain/Car.kt
+++ backend/src/main/kotlin/com/jellyone/lab1/domain/Car.kt
@@ -1,24 +1,9 @@
 package com.jellyone.lab1.domain
 
-import jakarta.persistence.*
-
-@Table(name = "car")
-@Entity
 data class Car(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    val id: Long?=null,
-
-    @Column(name = "color", nullable = false)
+    val id: Long? = null,
     val color: String = "",
-
-    @Column(name = "model", nullable = false)
     val model: String = "",
-
-    @Column(name = "brand", nullable = false)
     val brand: String = "",
-
-    @Column(name = "cool", nullable = false)
     val cool: Boolean = false
 )
