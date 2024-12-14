c970333a6b2abc58d2d2cda61c2d5264dad0995c
Egor Fedorov <fegor2004@gmail.com>
2024-12-14 13:29:35 +0300

    chore: revert to React 18
    This reverts commit fa86677cf72daeb413bd7215d4aa9cef7ebc984f.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,18 +3,10 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const ReactCompilerConfig = {}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", ReactCompilerConfig]
-        ]
-      }
-    }),
+    react(),
     TanStackRouterVite({
       routesDirectory: "./src/app/routes",
     }),
